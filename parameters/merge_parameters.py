2fb5293791e6e0ab291db7485e0f4ff4c4cebb25
Nayanika Das <nayanikadas0709@gmail.com>
2025-09-27 01:02:58 +0200

    all

--- parameters/merge_parameters.py
+++ parameters/merge_parameters.py
@@ -6,7 +6,7 @@ parameters.read_prm('qoplsaa.prm')
 parameters.read_prm('PAF.prm')
 parameters.read_prm('IND.prm')
 parameters.read_prm('IMI.prm')
-parameters.read_prm('WAT.prm')
+parameters.read_prm('HO2.prm')
 parameters.read_prm('PRD.prm')
 
 with open('qoplsaa_all.prm', 'w') as f:
